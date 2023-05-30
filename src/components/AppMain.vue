122f942f0ed74335294246dccfdc31e7c8807fb4
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-30 18:48:57 +0200

    stampa delle cards sul front dal back

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,14 +5,16 @@ export default {
     data() {
         return {
             projects: [],
+            baseUrl: "http://localhost:8000",
         }
     },
     methods: {
         getProjects() {
             console.log("hello");
 
-            axios.get("http://localhost:8000/api/projects")
+            axios.get(`${this.baseUrl}/api/projects`)
                 .then(response => {
+                    this.projects = response.data.results;
                     console.log(response);
                 });
         }
@@ -25,7 +27,15 @@ export default {
 
 <template>
     <div class="container">
-        <div class="row"></div>
+        <div class="row">
+            <div v-for="project in projects" class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ project.slug }}</h6>
+                    <p class="card-text">{{ project.content }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
