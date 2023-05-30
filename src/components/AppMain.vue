1e60f9ef179724e92922301385cd6ba8844b7877
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-30 18:13:08 +0200

    collegamento front con back tramite axios

---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from "axios";
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            projects: [],
+        }
+    },
+    methods: {
+        getProjects() {
+            console.log("hello");
+
+            axios.get("http://localhost:8000/api/projects")
+                .then(response => {
+                    console.log(response);
+                });
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row"></div>
+    </div>
+</template>
+
+<style lang="scss"></style>
